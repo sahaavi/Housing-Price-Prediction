36280ad78ccaa5c8158505d52e1c5a0cde87de15
Avishek Saha <avisheksaha123@gmail.com>
2023-07-06 23:50:56 -0700

    debug true

--- application.py
+++ application.py
@@ -35,4 +35,5 @@ def predict_datapoint():
         return render_template('predict.html', prediction=prediction[0])
     
 if __name__ == '__main__':
+    app.debug = True
     app.run(host='0.0.0.0', port=8080)
