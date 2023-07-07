80d2fde61dac41d9a027ccd5eae0c27b6cb733b0
Avishek Saha <avisheksaha123@gmail.com>
2023-07-07 00:12:10 -0700

    used dill instead of pickle

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 import numpy as np
-import pickle
+import dill
 # from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import mean_squared_error
@@ -17,7 +17,7 @@ def save_object(file_path, obj):
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
-            pickle.dump(obj, file_obj)
+            dill.dump(obj, file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
@@ -65,7 +65,7 @@ def evaluate_models(X_train, y_train,X_test, y_test, models, params):
 def load_object(file_path):
     try:
         with open(file_path, "rb") as file_obj:
-            obj = pickle.load(file_obj)
+            obj = dill.load(file_obj)
 
         return obj
     
